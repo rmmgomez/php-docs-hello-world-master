0668a8887e67f3aab5a608f5c6264d74bed98e65
Rosa Medina <rmmgomez@gmail.com>
2025-10-21 05:57:26 +0200

    actualizo

--- prueba_conexion.php
+++ prueba_conexion.php
@@ -28,8 +28,7 @@ try {
     $stmt = $pdo->query('SELECT NOW() AS fecha_actual;');
     $fila = $stmt->fetch();
     echo "Conectado correctamente. Hora del servidor: " .
-    $fila['fecha_actual'];
-    echo "Hola curso del CEFIRE";
+    $fila['fecha_actual'] . "Hola curso del CEFIRE";
 } catch (PDOException $e) {
     error_log('Error de conexión PDO: ' . $e->getMessage());
     echo "Error al conectar con la base de datos: " .
